e79275bf6630203cd1d2ec752c191528b15eb158
ajana-mograsys <ajana@mograsys.co.in>
2022-05-24 09:22:06 +0400

    new changes

---
+++ index.js
@@ -0,0 +1,107 @@
+//console.log("welcome to Javascript");
+//alert("be careful");
+//console.error("error message");
+//console.warn("warning");
+//document.write("welcome");
+
+//document.write("<h1>i love to learn a developer</h1>");//
+//var name;
+//name="Ajana";
+//var name="Ajana";
+//let age=33;
+//const pi=3.14;
+//document.write(name);
+//document.write("<br>");
+//document.write(age);
+//document.write("<br>");
+//document.write(typeof(name));
+//document.write("<br>");
+//document.write(typeof age);
+//document.write(typeof pi);
+//document.write(pi);
+//objects
+/*var student={
+            name:"ajana",
+             age:33,
+              place:"thrissur"
+            }                    
+console.log(student);
+document.write(student);
+document.write(student.name);*/
+//Arrays
+/*var months=['jan','feb','mar'];
+console.log(months);
+document.write(months[1]);
+var numbers=[1,2,3,4];
+
+document.write(numbers[3]);*/
+/*function welcome()//function definition
+{
+    document.write("welcome to js")
+}
+welcome(); //function call
+*/
+
+/*function sum(a,b){
+    let c=a+b;
+document.write("sums is"+c);//concatenation string with number by usinrg '+'
+}
+sum(20,30);
+sum(20,98);
+*/
+function display()
+{
+    alert("bre ready");
+}
+display();
+/*function pls(name)
+{
+    document.write("name of ="+name );
+
+}
+pls("ajana");*/
+/*function sum(  a, b)
+{
+    let sum=a+b;
+    document.write("sum of +"+sum);
+
+}
+sum(10,20);*/
+/*function farenheit(f)
+{
+    var c=(5/9) * (f-32)
+    document.write("temperature ="+c);
+    console.log("temp"+c);
+    
+}
+
+farenheit(102);
+*/
+
+
+/*let x = function (num) { return num * num };
+console.log(x(4));*/
+
+/*function square(a)
+{
+    let s=a*a;
+    document.write("square"+s);
+
+}
+square(4);*/
+
+/*function sum(a,b){
+
+    let s=a+b;
+    return s;
+
+}
+
+/*document.write("sum"+sum(2,3));
+
+
+    var add=new Function("num1","num2","return num1+num2");  
+    document.writeln(add(5,5));  
+for(let i=1;i<=5;i++)
+    document.write(i);*/
+    
